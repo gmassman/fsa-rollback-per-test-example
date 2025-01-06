10b480b37bf4c46f110df1e1e40f0110a698630f
Garrett Massman <gmassman20@gmail.com>
2025-01-06 08:58:31 -0800

    Update example conftest to push app_context sparingly

--- test/conftest.py
+++ test/conftest.py
@@ -4,8 +4,8 @@ from collections import namedtuple
 import pytest
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-from app import Device, create_app, User
-from app import db
+from app import db, create_app, Device, User
+
 
 os.environ["SQLALCHEMY_DATABASE_URI"] = (
     "postgresql://postgres:postgres@localhost/fsa_rollback_per_test_user"
@@ -14,59 +14,75 @@ os.environ["DEVICE_DB_URI"] = (
     "postgresql://postgres:postgres@localhost/fsa_rollback_per_test_device"
 )
 
+TEST_DB_BIND_KEYS = [None, "device_db"]  # only create required databases
+
 
 @pytest.fixture(scope="session")
-def test_client():
-    test_app = create_app()
-    test_client = test_app.test_client()
+def test_app():
+    app = create_app()
 
+    yield app
+
+
+@pytest.fixture
+def test_client(test_app):
     with test_app.app_context():
-        yield test_client
+        with test_app.test_client() as test_client:
+            yield test_client
 
 
 @pytest.fixture(scope="session")
-def database(test_client):
-    db.create_all()
+def database(test_app):
+    with test_app.app_context():
+        db.create_all()
 
-    yield db
+        yield db
 
-    db.drop_all()
+        db.drop_all()
 
 
 TxInfo = namedtuple("TxInfo", "connection,transaction")
 
 
 @pytest.fixture(autouse=True)
-def enable_transactional_tests(database):
-    """
-    Set up a transaction for each bind_key. The transaction will be rolled back at the end of each test.
-    This allows each test to begin with a clean database.
+def enable_transactional_tests(test_app, database):
+    """Set up a transaction for each bind_key. The transaction will be rolled
+    back at the end of each test. This allows each test to begin with a clean
+    database.
 
     Helpful links:
-    https://docs.sqlalchemy.org/en/20/orm/session_transaction.html#joining-a-session-into-an-external-transaction-such-as-for-test-suites
-    https://github.com/sqlalchemy/sqlalchemy/discussions/12176#discussioncomment-11549627
+    - https://docs.sqlalchemy.org/en/20/orm/session_transaction.html#joining-a-session-into-an-external-transaction-such-as-for-test-suites
+    - https://github.com/sqlalchemy/sqlalchemy/discussions/12176#discussioncomment-11549627
+    - https://github.com/pallets-eco/flask-sqlalchemy/issues/1171
     """
+
+    with test_app.app_context():
+        engines = db.engines
+
     tx_per_bind = {}
-    for bind_key, engine in database.engines.items():
+    for bind_key, engine in engines.items():
         connection = engine.connect()
         transaction = connection.begin()
         tx_per_bind[bind_key] = TxInfo(connection, transaction)
 
     binds = {}
-    for mapper in database.Model.registry.mappers:
+    for mapper in db.Model.registry.mappers:
         bind_key = getattr(mapper.class_, "__bind_key__", None)
-        binds[mapper] = tx_per_bind[bind_key].connection
+        if bind_key in TEST_DB_BIND_KEYS:
+            binds[mapper] = tx_per_bind[bind_key].connection
 
-    database.session = scoped_session(
+    db.session = scoped_session(
         session_factory=sessionmaker(
+            bind=tx_per_bind[None].connection,
             binds=binds,
             join_transaction_mode="create_savepoint",
+            autoflush=False,
         )
     )
 
     yield
 
-    database.session.close()
+    db.session.close()
     for tx in tx_per_bind.values():
         tx.transaction.rollback()
         tx.connection.close()
